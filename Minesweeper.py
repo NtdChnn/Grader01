f70abfa923d87e56e1c073b91bed72bf7394b679
Ntd Chnn <notdakon.may@gmail.com>
2021-08-13 12:48:45 +0700

    grader01

---
+++ Minesweeper.py
@@ -0,0 +1,36 @@
+def num_grid(lst):
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if lst[i][j] == '-':
+                lst[i][j] = 0
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if lst[i][j] == '#':
+                if i > 0 and isinstance(lst[i-1][j], int): lst[i-1][j] += 1
+                if j < 4 and isinstance(lst[i][j+1],int): lst[i][j+1] += 1
+                if j > 0 and isinstance(lst[i][j-1],int):lst[i][j-1] += 1
+                if i < 4 and isinstance(lst[i+1][j], int): lst[i+1][j] += 1
+                if i > 0 and j > 0 and isinstance(lst[i-1][j-1],int): lst[i-1][j-1] += 1
+                if i > 0 and j < 4 and isinstance(lst[i-1][j+1],int): lst[i-1][j+1] += 1
+                if i < 4 and j > 0 and isinstance(lst[i+1][j-1],int): lst[i+1][j-1] += 1
+                if i < 4 and j < 4 and isinstance(lst[i+1][j+1],int): lst[i+1][j+1] += 1
+
+    return lst
+
+'''lst_input = [
+    ["-","-","-","-","-"],
+    ["-","-","-","-","-"],
+    ["-","-","#","-","-"],
+    ["-","-","-","-","-"],
+    ["-","-","-","-","-"]
+]'''
+
+lst_input = []
+
+input_list = input().split(",")
+
+for e in input_list:
+  lst_input.append([i for i in e.split()])
+
+print("\n",*lst_input,sep = "\n")
+print("\n",*num_grid(lst_input),sep = "\n")
